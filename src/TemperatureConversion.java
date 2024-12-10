e9634ad8c224715ecd9325d9e78f96e3696b7947
shahmathossainmahin <shahmatmahin@gmail.com>
2024-12-11 00:33:29 +0600

    4th task submission

---
+++ src/TemperatureConversion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        Scanner myobj= new Scanner(System.in);
+        System.out.println("Enter the Celsius Temperature:");
+
+        float celsius = myobj.nextFloat();
+
+        double Fahrenheit =  ((9 * celsius)/ 5 ) + 32;
+
+
+            System.out.printf("The temperature in Fahrenheit scale is: %.2f%n",Fahrenheit );
+
+
+
+
+    }
+
+}
