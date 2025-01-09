7daaad8bafcd34c5a3f9916ceb93b3c1250eed9f
shahmathossainmahin <shahmatmahin@gmail.com>
2025-01-09 13:32:20 +0600

    task1.2.1

---
+++ src/PositiveInteger.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+public class PositiveInteger {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("Number is a positive integer.");
+        }
+        else {
+            System.out.println("Number is not a positive integer.");
+        }
+    }
+}
